29c6344eee9736d1bd37d3114986dcfd339fa8b0
Eslam Allam <eslamallam73@gmail.com>
2024-01-13 13:37:25 +0300

    feat: added database client

---
+++ lua/plugins/vim-dadbod-ui.lua
@@ -0,0 +1,22 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+
+  keys = {
+    { "<leader>dt", "<cmd>DBUIToggle<CR>", desc = "Toggle database client", mode = { "n" } },
+    { "<leader>da", "<cmd>DBUIAddConnection<CR>", desc = "Add database client connection", mode = { "n" } },
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
