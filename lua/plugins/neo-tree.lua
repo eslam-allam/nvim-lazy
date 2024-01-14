6ac24983105e520917594cff2a8d23cd5a43225a
Eslam Allam <eslamallam73@gmail.com>
2024-01-14 17:38:58 +0300

    feat: auto close neo-tree on file open

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+
+  opts = {
+    event_handlers = {
+
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          -- auto close
+          -- vimc.cmd("Neotree close")
+          -- OR
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    },
+  },
+}
