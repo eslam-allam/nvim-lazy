f026e7e2066d8e44a271a6c447fd32bd4f1bf9ed
Eslam Allam <eslamallam73@gmail.com>
2024-01-16 15:57:43 +0300

    feat: added project explorer to dashboard

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,63 @@
+return {
+  "nvimdev/dashboard-nvim",
+  opts = {
+    config = {
+      center = {
+        {
+          action = "Telescope project display_type=full",
+          desc = " Open project",
+          icon = "🗀 ",
+          key = "p",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New file",
+          icon = " ",
+          key = "n",
+        },
+        {
+          action = "Telescope oldfiles",
+          desc = " Recent files",
+          icon = " ",
+          key = "r",
+        },
+        {
+          action = "Telescope live_grep",
+          desc = " Find text",
+          icon = " ",
+          key = "g",
+        },
+        {
+          action = [[lua require("lazyvim.util").telescope.config_files()()]],
+          desc = " Config",
+          icon = " ",
+          key = "c",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = " ",
+          key = "s",
+        },
+        {
+          action = "LazyExtras",
+          desc = " Lazy Extras",
+          icon = " ",
+          key = "x",
+        },
+        {
+          action = "Lazy",
+          desc = " Lazy",
+          icon = "󰒲 ",
+          key = "l",
+        },
+        {
+          action = "qa",
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      },
+    },
+  },
+}
