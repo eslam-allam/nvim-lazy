247dc2d9bbf80c8187e05e31caf36d067502eaff
Eslam Allam <eslamallam73@gmail.com>
2024-01-16 12:41:47 +0300

    feat: added telescope branch explore

--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -4,8 +4,13 @@ return {
   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
 
   keys = {
-    { "<leader>fs", "<cmd>Telescope file_browser<CR>",  desc = "Telescope File Browser (root)", mode = { "n" }  },
-    { "<leader>fS", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",  desc = "Telescope File Browser (cwd)", mode = "n" },
+    { "<leader>fs", "<cmd>Telescope file_browser<CR>", desc = "Telescope File Browser (root)", mode = { "n" } },
+    {
+      "<leader>fS",
+      "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      desc = "Telescope File Browser (cwd)",
+      mode = "n",
+    },
   },
 
   config = function()
