c21350a669f6d17ec3c066193c850c56cf94a268
Eslam Allam <eslamallam73@gmail.com>
2024-01-16 15:21:28 +0300

    feat: activating venv on dir change

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged"}, {
   callback = function()
     local pythonFiles = vim.fn.globpath(vim.fn.getcwd(), "*.py", 0, 1)
     if #pythonFiles > 0 then
