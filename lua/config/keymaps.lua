dc231df5c2ec0747fee71b0af2ed8630dcf5729b
Eslam Allam <eslamallam73@gmail.com>
2024-01-22 01:15:52 +0300

    feat: added option to focus line diagnostic

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,7 @@
 local helpers = require("config.helpers")
 local Util = require("lazyvim.util")
 
+-- gradle
 vim.keymap.set("n", "<leader>tgqc", function()
   local root = helpers.User_configured_root_dir(vim.api.nvim_buf_get_name(0))
   local gradlew = helpers.User_configured_gradlew(root)
@@ -19,15 +20,19 @@ vim.keymap.set("n", "<leader>tgqc", function()
     vim.notify(output, 2, {})
   end
 end, { desc = "Kill current Gradle daemons" })
-
 vim.keymap.set("n", "<leader>tgqa", function()
   local _ = vim.fn.system({ "pkill", "-f", ".*GradleDaemon.*" })
   vim.notify("Succesfully killed all Gradle daemons", 2, {})
 end, { desc = "Kill all Gradle daemons" })
 
+-- lazygit
 vim.keymap.set("n", "<leader>gg", function() Util.terminal({ "lazygit" }, {cwd = Util.root(), esc_esc = false, ctrl_hjkl = false}) end, { desc = "Lazygit (Root)" })
 vim.keymap.set("n", "<leader>gG", function() Util.terminal({ "lazygit" }, {cwd = helpers.cwd(), esc_esc = false, ctrl_hjkl = false}) end, { desc = "Lazygit (cwd)" })
 
+-- terminal
 vim.keymap.set("n", "<leader>ft", function() Util.terminal(nil, { cwd = Util.root() }) end, { desc = "Terminal (Root)" })
 vim.keymap.set("n", "<leader>fT", function() Util.terminal(nil, { cwd = helpers.cwd() }) end, { desc = "Terminal (cwd)" })
 vim.keymap.set("n", "<C-/>", function() Util.terminal(nil, { cwd = Util.root() }) end, { desc = "Terminal (Root)" })
+
+-- diagnostic
+vim.keymap.set("n", "<leader>cD", function() vim.diagnostic.open_float(); vim.diagnostic.open_float() end, { desc = "Line Diagnostics (Focus)"})
