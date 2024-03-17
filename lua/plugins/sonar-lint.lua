86a71f620a2ebf295a5c1cf358d54e0cd52eaed8
Eslam Allam <eslamallam73@gmail.com>
2024-03-17 10:43:55 +0300

    fix: Stop jdtls from attempting to open in python files.
    This is a temporary fix and requires patches from the repo owner.

--- lua/plugins/sonar-lint.lua
+++ lua/plugins/sonar-lint.lua
@@ -1,29 +1,47 @@
 return {
-  url = "https://gitlab.com/schrieveslaach/sonarlint.nvim.git",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-jdtls"
-  },
-  name = "sonarlint",
-  ft = { "java" },
-  config = function()
-    require("sonarlint").setup({
-      server = {
-        cmd = {
-          "sonarlint-language-server",
-          -- Ensure that sonarlint-language-server uses stdio channel
-          "-stdio",
-          "-analyzers",
-          -- paths to the analyzers you need, using those for python and java in this example
-          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+  -- Have to configure once per lang as currently languages overlap.
+  {
+    url = "https://gitlab.com/schrieveslaach/sonarlint.nvim.git",
+    name = "sonarlint-java",
+    ft = "java",
+    config = function()
+      require("sonarlint").setup({
+        server = {
+          cmd = {
+            "sonarlint-language-server",
+            -- Ensure that sonarlint-language-server uses stdio channel
+            "-stdio",
+            "-analyzers",
+            -- paths to the analyzers you need, using those for python and java in this example
+            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+          },
         },
-      },
-      filetypes = {
-        "java",
-        "python"
-      }
-    })
-  end,
+        filetypes = {
+          "java",
+        },
+      })
+    end,
+  },
+  {
+    url = "https://gitlab.com/schrieveslaach/sonarlint.nvim.git",
+    name = "sonarlint-python",
+    ft = "python",
+    config = function()
+      require("sonarlint").setup({
+        server = {
+          cmd = {
+            "sonarlint-language-server",
+            -- Ensure that sonarlint-language-server uses stdio channel
+            "-stdio",
+            "-analyzers",
+            -- paths to the analyzers you need, using those for python and java in this example
+            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+          },
+        },
+        filetypes = {
+          "python",
+        },
+      })
+    end,
+  },
 }
