c982cffd829487e1c6087c57b8423d415cac493c
Eslam Allam <eslamallam73@gmail.com>
2024-03-25 10:09:02 +0300

    feat: Add mason and treesitter ensure installed

---
+++ lua/plugins/langs.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      --- Java
+      vim.list_extend(opts.ensure_installed, { "sonarlint-language-server", "gradle-language-server", "jdtls" })
+
+      --- Bash
+      vim.list_extend(opts.ensure_installed, { "bash-language-server" })
+
+      -- Docker
+      vim.list_extend(opts.ensure_installed, { "docker-compose-language-service", "dockerfile-language-server" })
+
+      -- PHP
+      vim.list_extend(opts.ensure_installed, { "intelephense" })
+
+      -- Config files
+      vim.list_extend(
+        opts.ensure_installed,
+        { "yaml-language-server", "json-lsp", "prettierd", "stylua", "yamllint", "jsonlint", "taplo" }
+      )
+
+      -- Svelte
+      vim.list_extend(
+        opts.ensure_installed,
+        { "svelte-language-server", "tailwindcss-language-server", "typescript-language-server" }
+      )
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "svelte", "css", "typescript", "javascript", "scss" })
+      end
+    end,
+  },
+}
