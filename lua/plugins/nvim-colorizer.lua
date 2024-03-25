43485f08987cf230af46ef484118fe6e87733ff4
Eslam Allam <eslamallam73@gmail.com>
2024-03-26 02:48:00 +0300

    feat: Enable all css features in colorizer

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -4,7 +4,13 @@ return {
   keys = {
     { "<leader>ch", "<cmd>ColorizerToggle<cr>", desc = "Toggle colorizer" },
   },
-  opts = {
-    "*",
-  },
+  config = function()
+    require("colorizer").setup({
+      "*";
+      css = { css = true },
+      svelte = { css = true },
+      javascript = { css = true },
+      typescript = { css = true },
+    }, { names = false })
+  end,
 }
