353d55e5224e82edd64b76dc391301dd2d46451d
Eslam Allam <eslamallam73@gmail.com>
2024-03-29 05:32:19 +0300

    feat: Add troubleV3 and refactor colorscheme config

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+    },
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
