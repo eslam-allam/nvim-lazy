626d2109d75113941d51c976c94e50a92e1aac6b
Eslam Allam <eslamallam73@gmail.com>
2024-04-12 14:18:10 +0300

    fix: Use default config home if env not set

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,7 +9,9 @@ vim.g.python3_host_prog = "~/miniconda3/bin/python"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
-vim.env.JAVA_RUNTIMES_JSON = os.getenv("XDG_CONFIG_HOME") .. "/nvim/config-files/jdtls/java-runtimes.json"
-vim.env.CUSTOM_JAVA_ROOTS = os.getenv("XDG_CONFIG_HOME") .. "/nvim/config-files/jdtls/jdtls-custom-roots.json"
+local config_home = os.getenv("XDG_CONFIG_HOME") or "~/.config"
+
+vim.env.JAVA_RUNTIMES_JSON = config_home .. "/nvim/config-files/jdtls/java-runtimes.json"
+vim.env.CUSTOM_JAVA_ROOTS = config_home .. "/nvim/config-files/jdtls/jdtls-custom-roots.json"
 
 vim.g.root_spec = { "lsp", { ".git" }, "cwd" }
