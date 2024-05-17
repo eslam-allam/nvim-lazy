c27e6763b568e906849164d121d7ed3464803de1
Eslam Allam <eslamallam73@gmail.com>
2024-05-17 21:31:50 +0300

    feat: Adjust snippet keybinds

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -9,4 +9,19 @@ return {
       },
     },
   },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = {
+      {
+        "<C-Tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true, silent = true, mode = "i",
+      },
+      { "<tab>", "<tab>", mode = "i" },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    },
+  }
 }
