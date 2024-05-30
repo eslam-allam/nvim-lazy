abc0385377b2201ff44d9ea09d4002c6a54c422e
Eslam Allam <eslamallam73@gmail.com>
2024-05-30 10:24:58 +0300

    feat: Add diff view

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Diff View", mode = "n" },
+    { "<leader>gf", "<cmd>DiffviewFileHistory %<CR>", desc = "File History (This)", mode = "n" },
+    { "<leader>gF", "<cmd>DiffviewFileHistory<CR>", desc = "File History (All)", mode = "n" },
+  }
+}
