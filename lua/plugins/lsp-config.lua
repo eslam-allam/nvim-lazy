5713851f47fa229483858f406f65492c8b433034
Eslam Allam <eslamallam73@gmail.com>
2024-06-02 19:30:06 +0300

    fix(workaround): Ignore document version when applying lsp edits
    Will be reverted later once the issue is fixed upstream

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,12 @@
+vim.lsp.util.apply_text_document_edit = function(text_document_edit, index, offset_encoding)
+    local text_document = text_document_edit.textDocument
+    local bufnr = vim.uri_to_bufnr(text_document.uri)
+    if offset_encoding == nil then
+        vim.notify_once('apply_text_document_edit must be called with valid offset encoding', vim.log.levels.WARN)
+    end
+
+    vim.lsp.util.apply_text_edits(text_document_edit.edits, bufnr, offset_encoding)
+end
 return {
   {
     "neovim/nvim-lspconfig",
