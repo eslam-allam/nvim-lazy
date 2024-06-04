f5e6f2ca829ee2c546a4946622022cd4c3065006
Eslam Allam <eslamallam73@gmail.com>
2024-06-04 14:50:04 +0300

    feat: Adjust snippet jump keybinds

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,6 +2,40 @@ local cmp = require("cmp")
 return {
   {
     "nvim-cmp",
+    keys = function()
+      return {
+        {
+          "<c-tab>",
+          function()
+            return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next"
+              or vim.notify("[LuaSnip] No more Results", vim.log.levels.WARN)
+          end,
+          expr = true,
+          silent = true,
+          mode = { "i", "s" },
+        },
+        {
+          "<tab>",
+          function()
+            return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next"
+              or vim.notify("[LuaSnip] No more Results", vim.log.levels.WARN)
+          end,
+          expr = true,
+          silent = true,
+          mode = { "s" },
+        },
+        {
+          "<s-tab>",
+          function()
+            return require("luasnip").jumpable(-1) and "<Plug>luasnip-jump-prev"
+              or vim.notify("[LuaSnip] No more Results", vim.log.levels.WARN)
+          end,
+          expr = true,
+          silent = true,
+          mode = { "i", "s" },
+        },
+      }
+    end,
     opts = {
       window = {
         completion = cmp.config.window.bordered(),
