8b220406995f98e1108d1f27ba0df7703a6a3d44
Eslam Allam <eslamallam73@gmail.com>
2024-06-10 14:00:54 +0300

    feat: Add indentscope and context-commentstring

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,13 @@
 return {
+  dependencies = {
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      lazy = true,
+      opts = {
+        enable_autocmd = false,
+      },
+    },
+  },
   "numToStr/Comment.nvim",
   config = true,
 }
