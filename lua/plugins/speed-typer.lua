ae17a8e0dbe5b9a07b12e838e70cf3fb80ec7fa9
Eslam Allam <eslamallam73@gmail.com>
2024-06-22 18:25:19 +0300

    feat: Add speedtyper plugin

---
+++ lua/plugins/speed-typer.lua
@@ -0,0 +1,12 @@
+return {
+  "NStefan002/speedtyper.nvim",
+  cmd = "Speedtyper",
+  keys = {
+    { "<leader>pt", "<cmd>Speedtyper<CR>", mode = "n", desc = "Typing Test" },
+  },
+  opts = {
+    window = {
+      close_with = { n = "q", i = "<M-q>" },
+    },
+  },
+}
