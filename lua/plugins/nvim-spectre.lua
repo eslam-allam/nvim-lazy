0658ec7a5ca27c5c681c750eddb9d3c66bc13bf4
Eslam Allam <eslamallam73@gmail.com>
2024-06-29 02:14:29 +0300

    feat: Add nvim-rip-substitute for buffer find and replace

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -1,7 +1,15 @@
 return {
   "nvim-pack/nvim-spectre",
-  keys = {
-    {"<leader>sf", function() require("spectre").open_file_search({select_word=true}) end, desc="Spectre File (Current Word)", mode={"n"}},
-    {"<leader>sF", function() require("spectre").open_file_search() end, desc="Spectre File (Selected)", mode={"n", "x"}},
-  }
+  keys = function()
+    return {
+      {
+        "<leader>sF",
+        function()
+          require("spectre").open_file_search()
+        end,
+        desc = "Spectre File (Selected)",
+        mode = { "n", "x" },
+      },
+    }
+  end,
 }
---
+++ lua/plugins/rip-sub.lua
@@ -0,0 +1,16 @@
+return {
+	"chrisgrieser/nvim-rip-substitute",
+	keys = {
+		{
+			"<leader>sf",
+			function() require("rip-substitute").sub() end,
+			mode = { "n", "x" },
+			desc = " rip substitute",
+		},
+	},
+  opts = {
+    popupWin = {
+      position = "top"
+    }
+  }
+}
