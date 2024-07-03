1e6c6bfc6b197040af5e25d8200d6f58d7b265d5
Eslam Allam <eslamallam73@gmail.com>
2024-07-03 23:06:50 +0300

    fix: Add templ to tailwindcss included languages
    Previous config no longer working after 0.17 update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,34 +1,32 @@
-local helpers = require("modules.helpers")
-vim.lsp.util.apply_text_document_edit = function(text_document_edit, index, offset_encoding)
-  local text_document = text_document_edit.textDocument
-  local bufnr = vim.uri_to_bufnr(text_document.uri)
-  if offset_encoding == nil then
-    vim.notify_once("apply_text_document_edit must be called with valid offset encoding", vim.log.levels.WARN)
-  end
-
-  vim.lsp.util.apply_text_edits(text_document_edit.edits, bufnr, offset_encoding)
-end
 return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
+      local helpers = require("modules.helpers")
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = {
         "K",
         function()
-          require("modules.helpers").lspRequestExcludeLsps(vim.lsp.protocol.Methods.textDocument_hover, vim.g.hover_exclude_lsps)
+          helpers.lspRequestExcludeLsps(vim.lsp.protocol.Methods.textDocument_hover, vim.g.hover_exclude_lsps)
         end,
       }
-      keys[#keys+1] = {
+      keys[#keys + 1] = {
         "gd",
-        function ()
-          require("modules.helpers").lspRequestExcludeLsps(vim.lsp.protocol.Methods.textDocument_definition, vim.g.definition_exclude_lsps)
-        end
+        function()
+          helpers.lspRequestExcludeLsps(vim.lsp.protocol.Methods.textDocument_definition, vim.g.definition_exclude_lsps)
+        end,
       }
       -- make sure mason installs the server
       opts.servers.gradle_ls = {}
-      opts.setup.gradle_ls = function(_, optss)
-        optss.root_dir = require("modules.java").javaRoot
+      opts.setup.gradle_ls = function(_, sopts)
+        sopts.root_dir = require("modules.java").javaRoot
+      end
+      opts.setup.tailwindcss = function(_, sopts)
+        sopts.settings = {
+          tailwindCSS = {
+            includeLanguages = { templ = "html" },
+          },
+        }
       end
     end,
   },
