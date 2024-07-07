f01fa06993ee8c9cef9a9fb4ebb7cf7e0e47c876
Eslam Allam <eslamallam73@gmail.com>
2024-07-07 20:32:20 +0300

    feat: Add more icons to mini icons

---
+++ lua/plugins/icons.lua
@@ -0,0 +1,21 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      ["favicon.ico"] = { glyph = "", hl = "MiniIconsPurple" },
+      gradlew = { glyph = "", hl = "MiniIconsGreen" },
+    },
+    extension = {
+      woff = { glyph = "", hl = "MiniIconsAzure" },
+      otf = { glyph = "", hl = "MiniIconsAzure" },
+      ttf = { glyph = "", hl = "MiniIconsAzure" },
+      ["js.map"] = { glyph = "", hl = "MiniIconsOrange" },
+      log = { glyph = "󰌱", hl = "MiniIconsAzure" },
+      db = { glyph = "󰆼", hl = "MiniIconsGrey" },
+      jar = { glyph = "", hl = "MiniIconsOrange" },
+    },
+    filetype = {
+      templ = { glyph = "", hl = "MiniIconsYellow" },
+    },
+  },
+}
