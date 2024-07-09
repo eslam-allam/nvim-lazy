ed3ce3bf5f8498eaad3a92b43dd200688e375d1c
Eslam Allam <eslamallam73@gmail.com>
2024-07-09 23:11:06 +0300

    feat: Use catpuccin bat theme for code screenshots

--- lua/plugins/snap.lua
+++ lua/plugins/snap.lua
@@ -2,6 +2,7 @@ return {
   "eslam-allam/snap.nvim",
   build = ":SnapBuild",
   opts = {
+    theme = "tmTheme://~/.config/bat/themes/Catppuccin Mocha.tmTheme",
     line_offset = true,
     watermark = {
       font = "JetBrainsMono-NF-Regular",
@@ -9,7 +10,7 @@ return {
       text = " eslam-allam",
       position = "SouthEast",
       font_size = 15,
-      opacity = 0.8
-    }
+      opacity = 0.8,
+    },
   },
 }
