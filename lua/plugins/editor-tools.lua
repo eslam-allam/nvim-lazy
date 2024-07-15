d593a513735824df2a5185286187a0f86d01b6c2
Eslam Allam <eslamallam73@gmail.com>
2024-07-15 11:04:56 +0300

    feat: Add dial.nvim

---
+++ lua/plugins/editor-tools.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "monaqa/dial.nvim",
+    keys = function()
+      ---@param increment boolean
+      ---@param g? boolean
+      local function dial(increment, g)
+        local mode = vim.fn.mode(true)
+        -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
+        local is_visual = mode == "v" or mode == "V" or mode == "\22"
+        local func = (increment and "inc" or "dec") .. (g and "_g" or "_") .. (is_visual and "visual" or "normal")
+        local group = vim.g.dials_by_ft[vim.bo.filetype] or "default"
+        return require("dial.map")[func](group)
+      end
+      return {
+        -- stylua: ignore
+        { "<C-g>", function() return dial(true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
+        -- stylua: ignore
+        { "<C-x>", function() return dial(false) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
+        -- stylua: ignore
+        { "g<C-g>", function() return dial(true, true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
+        -- stylua: ignore
+        { "g<C-x>", function() return dial(false, true) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
+      }
+    end,
+  },
+}
