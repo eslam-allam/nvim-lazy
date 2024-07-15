eb887697c85205f35cbd8c9d6d3d15ca9f450206
Eslam Allam <eslamallam73@gmail.com>
2024-07-15 18:15:47 +0300

    fix: Improve octo keymaps

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -3,22 +3,25 @@ return {
   opts = {
     mappings = {
       review_diff = {
-        add_review_comment = { lhs = "grc", desc = "add a new review comment" },
-        add_review_suggestion = { lhs = "grs", desc = "add a new review suggestion" },
+        add_review_comment = { lhs = "goc", desc = "add a new review comment" },
+        add_review_suggestion = { lhs = "gos", desc = "add a new review suggestion" },
       },
       review_thread = {
-        add_comment = { lhs = "grca", desc = "add a new review comment" },
-        add_suggestion = { lhs = "grs", desc = "add a new review suggestion" },
-        delete_comment = { lhs = "grcd", desc = "delete comment" },
+        add_comment = { lhs = "goca", desc = "add a new review comment" },
+        add_suggestion = { lhs = "gos", desc = "add a new review suggestion" },
+        delete_comment = { lhs = "gocd", desc = "delete comment" },
       },
       issue = {
-        add_comment = { lhs = "gica", desc = "add comment" },
-        delete_comment = { lhs = "gicd", desc = "delete comment" },
+        add_comment = { lhs = "goca", desc = "add comment" },
+        delete_comment = { lhs = "gocd", desc = "delete comment" },
       },
       pull_request = {
-        add_comment = { lhs = "gpca", desc = "add comment" },
-        delete_comment = { lhs = "gpcd", desc = "delete comment" },
+        add_comment = { lhs = "goca", desc = "add comment" },
+        delete_comment = { lhs = "gocd", desc = "delete comment" },
       },
+      submit_win = {
+        approve_review = { lhs = "<C-g>", desc = "approve review" },
+      }
     },
   },
 }
