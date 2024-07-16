b64d56fa69c58c7fb9fe24cdd2ad0b39a0707d95
Eslam Allam <eslamallam73@gmail.com>
2024-07-17 02:21:53 +0300

    feat: Format injected javascript with prettierd

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,16 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      templ = { "templ" },
+      templ = { "templ", "injected" },
+    },
+    formatters = {
+      injected = {
+        options = {
+          lang_to_formatters = {
+            javascript = { "prettierd" },
+          },
+        },
+      },
     },
   },
 }
