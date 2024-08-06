7bf6145e7a5ae33142ed57bd789e7d172df95b6c
Eslam Allam <eslamallam73@gmail.com>
2024-08-06 05:09:54 +0300

    feat: Add more kulal keymaps and icons

---
+++ lua/plugins/tools/kuala.lua
@@ -0,0 +1,70 @@
+return {
+  "mistweaverco/kulala.nvim",
+  keys = function()
+    return {}
+  end,
+  config = function(_, opts)
+    require("kulala").setup(opts)
+    require("which-key").add({
+      "<leader>R",
+      desc = "Rest",
+      icon = { cat = "filetype", name = "http" },
+      {
+        "<leader>Rc",
+        function()
+          require("kulala").close()
+        end,
+        desc = "Close Window",
+        icon = { icon = "󰆴", color = "red" },
+      },
+      {
+        "<leader>Rt",
+        function()
+          require("kulala").scratchpad()
+        end,
+        desc = "Open Scra[t]chpad",
+        icon = { icon = "󰎚", color = "yellow" },
+      },
+      {
+        "<leader>Rv",
+        function()
+          require("kulala").set_selected_env()
+        end,
+        desc = "Set Env",
+        icon = { cat = "filetype", name = "dotenv" },
+      },
+      {
+        "<leader>RS",
+        function()
+          require("kulala").search()
+        end,
+        desc = "Find Files",
+        icon = { icon = "", color = "green" },
+      },
+      {
+        "<leader>Rn",
+        function()
+          require("kulala").jump_next()
+        end,
+        desc = "Jump to next request",
+        icon = { icon = "󰈑", color = "yellow" },
+      },
+      {
+        "<leader>Rp",
+        function()
+          require("kulala").jump_prev()
+        end,
+        desc = "Jump to previous request",
+        icon = { icon = "", color = "yellow" },
+      },
+      {
+        "<leader>Rs",
+        function()
+          require("kulala").run()
+        end,
+        desc = "Send the request",
+        icon = { icon = "", color = "green" },
+      },
+    })
+  end,
+}
