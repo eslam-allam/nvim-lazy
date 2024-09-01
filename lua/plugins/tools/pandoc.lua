b05b99fed48b73d44484308f5b5e48d6c25356c2
Eslam Allam <eslamallam73@gmail.com>
2024-09-01 15:25:02 +0300

    refactor: Use which key for pandoc mappings

--- lua/plugins/tools/pandoc.lua
+++ lua/plugins/tools/pandoc.lua
@@ -1,51 +1,55 @@
 return {
   "aspeddro/pandoc.nvim",
   init = function()
-    require("modules.pandoc").setup()
-  end,
-  keys = {
-    {
+    require("which-key").add({
       "<leader>P",
-      function()
-        local directory = vim.fn.expand("%:p:h")
-        local home = os.getenv("HOME")
+      desc = "Pandoc",
+      icon = { cat = "filetype", name = "pdf" },
+      {
+        "<leader>Pp",
+        function()
+          require("pandoc.render").file(require("modules.pandoc").command())
+        end,
+        desc = "Render PDF (cwd)",
+        mode = { "n" },
+      },
+      {
+        "<leader>PP",
+        function()
+          local directory = vim.fn.expand("%:p:h")
+          local home = os.getenv("HOME")
 
-        if home == nil then
-          vim.notify("Could not find home directory", 4)
-          return
-        end
-        local directoryWithoutHome = string.sub(directory, string.len(home) + 2)
+          if home == nil then
+            vim.notify("Could not find home directory", 4)
+            return
+          end
+          local directoryWithoutHome = string.sub(directory, string.len(home) + 2)
 
-        local outputDirectory = home .. "/Documents/pandoc/" .. directoryWithoutHome
-        local fileName = vim.fn.expand("%:t")
+          local outputDirectory = home .. "/Documents/pandoc/" .. directoryWithoutHome
+          local fileName = vim.fn.expand("%:t")
 
-        local extension = string.sub(fileName, string.len(fileName) - 1)
+          local extension = string.sub(fileName, string.len(fileName) - 1)
 
-        if extension ~= "md" then
-          vim.notify("[Pandoc] File is not Markdown", 4)
-          return
-        end
-        local newName = string.sub(fileName, 0, string.len(fileName) - 2) .. "pdf"
+          if extension ~= "md" then
+            vim.notify("[Pandoc] File is not Markdown", 4)
+            return
+          end
+          local newName = string.sub(fileName, 0, string.len(fileName) - 2) .. "pdf"
 
-        vim.system({ "mkdir", "-p", outputDirectory })
+          vim.system({ "mkdir", "-p", outputDirectory })
 
-        require("pandoc.render").file(
-          require("modules.pandoc").command({ ["--output"] = outputDirectory .. "/" .. newName })
-        )
-      end,
-      desc = "Render PDF (Documents Dir)",
-      mode = { "n" },
-      ft = "markdown",
-    },
-    {
-      "<leader>p",
-      function()
-        require("pandoc.render").file(require("modules.pandoc").command())
-      end,
-      desc = "Render PDF (cwd)",
-      mode = { "n" },
-      ft = "markdown",
-    },
-  },
-  opts = {},
+          require("pandoc.render").file(
+            require("modules.pandoc").command({ ["--output"] = outputDirectory .. "/" .. newName })
+          )
+        end,
+        desc = "Render PDF (Documents Dir)",
+        mode = { "n" },
+      },
+    }, {})
+  end,
+  config = function()
+    require("modules.pandoc").setup({ code_theme = "catppuccin" })
+    require("pandoc").setup()
+  end,
+  lazy = true,
 }
