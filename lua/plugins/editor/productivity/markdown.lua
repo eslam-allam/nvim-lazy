f56fa897d778e5cbc3fa360c52948417ab8ff63c
Eslam Allam <eslamallam73@gmail.com>
2024-09-15 17:33:25 +0300

    feat: Add table-nvim and checkbox-cycle for markdown files

---
+++ lua/plugins/editor/productivity/markdown.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "SCJangra/table-nvim",
+    ft = "markdown",
+    opts = {},
+  },
+  {
+    "epilande/checkbox-cycle.nvim",
+    ft = "markdown",
+    opts = {
+      states = { "[ ]", "[x]" },
+    },
+    keys = {
+      {
+        "<CR>",
+        "<Cmd>CheckboxCycleNext<CR>",
+        desc = "Checkbox Next",
+        ft = { "markdown" },
+        mode = { "n", "v" },
+      },
+      {
+        "<S-CR>",
+        "<Cmd>CheckboxCyclePrev<CR>",
+        desc = "Checkbox Previous",
+        ft = { "markdown" },
+        mode = { "n", "v" },
+      },
+    },
+  },
+}
