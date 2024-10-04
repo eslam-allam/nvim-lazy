d3099a516e65eb324c980bdc12ba53abeac66b83
Eslam Allam <eslamallam73@gmail.com>
2024-10-04 21:35:01 +0300

    feat: Implement nvchad like telescope colors

--- lua/plugins/editor/looks/themes.lua
+++ lua/plugins/editor/looks/themes.lua
@@ -9,6 +9,27 @@ return {
     "catppuccin/nvim",
     lazy = false,
     priotity = 1000,
+    opts = function(_, opts)
+      local colors = require("catppuccin.palettes").get_palette("mocha")
+      local localOpts = {
+        custom_highlights = {
+          TelescopeMatching = { fg = colors.flamingo },
+          TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+
+          TelescopePromptPrefix = { bg = colors.surface0 },
+          TelescopePromptNormal = { bg = colors.surface0 },
+          TelescopeResultsNormal = { bg = colors.mantle },
+          TelescopePreviewNormal = { bg = colors.mantle },
+          TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+          TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+          TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
+          TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
+          TelescopeResultsTitle = { fg = colors.mantle },
+          TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
+        },
+      }
+      return vim.tbl_extend("force", opts, localOpts)
+    end,
   },
   {
     "LazyVim/LazyVim",
