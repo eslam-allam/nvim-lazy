fe45a6bd7c708f331eddfb5cfc51b56be0f84c35
Eslam Allam <eslamallam73@gmail.com>
2024-10-04 21:23:58 +0300

    feat: Add minty and remove deadcolumn

--- lua/plugins/editor/looks/editor-decorations.lua
+++ lua/plugins/editor/looks/editor-decorations.lua
@@ -6,16 +6,6 @@ return {
     },
   },
   {
-    "Bekaboo/deadcolumn.nvim",
-    config = function(_, _)
-      vim.api.nvim_create_autocmd("FileType", {
-        desc = "deadcolumn config",
-        pattern = { "java", "go", "python", "lua" },
-        command = "setlocal colorcolumn=80",
-      })
-    end,
-  },
-  {
     "mvllow/modes.nvim",
     opts = {
       set_number = false,
---
+++ lua/plugins/tools/minty.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvchad/minty",
+    lazy = true,
+    dependencies = {
+      { "nvchad/volt", lazy = true },
+    },
+    init = function()
+      require("which-key").add({
+        "<localleader>c",
+        group = "Color",
+        {
+          "<localleader>cp",
+          group = "Picker",
+          { "<localleader>cph", function() require("minty.huefy").open() end,desc = "Pick Hue" },
+          { "<localleader>cps", function() require("minty.shades").open() end, desc = "Pick Shade" },
+        },
+      })
+    end,
+  },
+}
