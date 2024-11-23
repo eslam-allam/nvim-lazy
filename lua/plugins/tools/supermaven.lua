2eb6fb2f0bd9ef196adaad818836f6ed80f6aecf
Eslam Allam <eslamallam73@gmail.com>
2024-11-23 20:54:39 +0300

    feat: Add supermaven extra

--- lua/plugins/tools/supermaven.lua
+++ lua/plugins/tools/supermaven.lua
@@ -1,21 +1,11 @@
 return {
   {
     "supermaven-inc/supermaven-nvim",
-    dependencies = {
-      {
-        "nvim-cmp",
-        opts = function(_, opts)
-          require("modules.completion-styler").addIcon("Supermaven", "󱚟")
-          table.insert(opts.sources, { name = "supermaven" })
-          local colors = require("catppuccin.palettes").get_palette("mocha")
-          vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = colors.blue })
-        end,
-      },
-    },
-    config = true,
-    opts = {
-      disable_keymaps = true,
-      disable_inline_completion = true,
-    },
+    config = function(_, opts)
+      require("modules.completion-styler").addIcon("Supermaven", "󱚟")
+      local colors = require("catppuccin.palettes").get_palette("mocha")
+      vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = colors.blue })
+      require("supermaven-nvim").setup(opts)
+    end,
   },
 }
