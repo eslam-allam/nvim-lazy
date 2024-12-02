c76835c8e329f46e96413bb239adcfc50b712802
Eslam Atef Allam <eslam.allam@aubh.edu.bh>
2024-12-02 15:27:06 +0300

    fix: Add neovide specific keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -80,9 +80,12 @@ vim.keymap.set("n", "<leader>cD", function()
   vim.diagnostic.open_float()
 end, { desc = "Line Diagnostics (Focus)" })
 
--- Disabled
-vim.keymap.del("n", "grr")
-vim.keymap.del("n", "gra")
-vim.keymap.del("n", "grn")
+if vim.g.neovide then
+  -- Allow clipboard copy paste in neovim
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+end
 
 -- Plugins
