c1f1e5ac6fa1dfdf4223d59eaac76ef878066f40
Eslam Atef Allam <eslam.allam@aubh.edu.bh>
2024-12-02 14:40:22 +0300

    fix: Add windows compatability

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
+
+if vim.fn.has("windows") == 1 then
+  vim.opt.shell = '"C:/Program Files/Git/bin/bash.exe"'
+end
+
 vim.g.python3_host_prog = "~/miniconda3/envs/nvim/bin/python"
 
 vim.g.maplocalleader = ","
@@ -11,12 +17,11 @@ vim.g.autoformat = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
-local config_home = os.getenv("XDG_CONFIG_HOME") or "~/.config"
+local config_home = vim.fn.stdpath("config")
 
+vim.env.FORMATTER_SCHEMA_DIR = config_home .. "/config-files/formatter-schemas"
 
-vim.env.FORMATTER_SCHEMA_DIR = config_home .. "/nvim/config-files/formatter-schemas"
-
-local jdtls_config_dir = config_home .. "/nvim/config-files/jdtls"
+local jdtls_config_dir = config_home .. "/config-files/jdtls"
 vim.env.JAVA_RUNTIMES_JSON = jdtls_config_dir .. "/java-runtimes.json"
 vim.env.JAVA_RUNTIMES_JSON_SCHEMA = jdtls_config_dir .. "/java-runtimes.schema.json"
 vim.env.CUSTOM_JAVA_ROOTS = jdtls_config_dir .. "/jdtls-custom-roots.json"
@@ -32,4 +37,4 @@ vim.g.definition_exclude_lsps = {
   { name = "jdtls", filetypes = { "java" }, ts_captures = { "string" } },
 }
 
-vim.g.custom_formater_exec_folder = vim.fn.expand('~/.local/share/nvim/conform/custom_formatters')
+vim.g.custom_formater_exec_folder = vim.fn.expand("~/.local/share/nvim/conform/custom_formatters")
--- lua/plugins/lang/venv-selector.lua
+++ lua/plugins/lang/venv-selector.lua
@@ -1,13 +1,24 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  opts = {
-    settings = {
-      search = {
-        miniconda = {
-          command = "fd '/envs/.*/bin/python$' " .. os.getenv("CONDA_PREFIX") .. " --full-path",
-          type = "anaconda"
-        }
-      }
-    }
-  },
+
+  opts = function(_, opts)
+    local conda_prefix = os.getenv("CONDA_PREFIX")
+    if not conda_prefix then
+      if vim.fn.has("windows") == 1 then
+        conda_prefix = vim.fn.expandcmd("~/AppData/Local/miniconda3")
+      else
+        error("conda_prefix not found")
+      end
+    end
+    return vim.tbl_deep_extend("keep", {
+      settings = {
+        search = {
+          miniconda = {
+            command = "fd '/envs/.*/bin/python$' " .. conda_prefix .. " --full-path",
+            type = "anaconda",
+          },
+        },
+      },
+    }, opts)
+  end,
 }
