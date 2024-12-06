276c611c8ff4f55b5882a12823d4d4df9e942ae6
Eslam Allam <eslamallam73@gmail.com>
2024-12-06 17:19:44 +0300

    feat: Add image preview capability to telescope

--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -74,6 +74,36 @@ return {
     },
 
     defaults = {
+      preview = {
+        mime_hook = function(filepath, bufnr, opts)
+          local chafa_installed = vim.fn.executable("chafa")
+          if not chafa_installed then
+            vim.notify_once("Chafa is not installed. Please install it to preview images.", vim.log.levels.WARN)
+          end
+          local is_image = function(filepath)
+            local image_extensions = { "png", "jpg", "webp" } -- Supported image formats
+            local split_path = vim.split(filepath:lower(), ".", { plain = true })
+            local extension = split_path[#split_path]
+            return vim.tbl_contains(image_extensions, extension)
+          end
+          if chafa_installed and is_image(filepath) then
+            local term = vim.api.nvim_open_term(bufnr, {})
+            local function send_output(_, data, _)
+              for _, d in ipairs(data) do
+                vim.api.nvim_chan_send(term, d .. "\r\n")
+              end
+            end
+            vim.fn.jobstart({
+              "chafa",
+              "-f",
+              "symbols",
+              filepath, -- Terminal image viewer command
+            }, { on_stdout = send_output, stdout_buffered = true, pty = true })
+          else
+            require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
+          end
+        end,
+      },
       path_display = { filename_first = { reverse_directories = false } },
       mappings = {
         i = {
