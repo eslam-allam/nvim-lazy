84831fab8dffd30ee0e45ad2356ef38cbbfaf8e5
Eslam Allam <eslamallam73@gmail.com>
2024-12-06 15:28:41 +0300

    feat: Only use kitty branch symbol when using kitty

--- lua/plugins/editor/git/gitgraph.lua
+++ lua/plugins/editor/git/gitgraph.lua
@@ -1,51 +1,53 @@
 return {
   "isakbm/gitgraph.nvim",
   dependencies = { "sindrets/diffview.nvim" },
-  ---@type I.GGConfig
-  opts = {
-    symbols = {
-      merge_commit = "",
-      commit = "",
-      merge_commit_end = "",
-      commit_end = "",
+  opts = function()
+    local is_kitty = vim.fn.getenv("KITTY_WINDOW_ID") ~= vim.v.null
+    return {
+      symbols = not is_kitty and {} or {
+        merge_commit = "",
+        commit = "",
+        merge_commit_end = "",
+        commit_end = "",
 
-      -- Advanced symbols
-      GVER = "",
-      GHOR = "",
-      GCLD = "",
-      GCRD = "╭",
-      GCLU = "",
-      GCRU = "",
-      GLRU = "",
-      GLRD = "",
-      GLUD = "",
-      GRUD = "",
-      GFORKU = "",
-      GFORKD = "",
-      GRUDCD = "",
-      GRUDCU = "",
-      GLUDCD = "",
-      GLUDCU = "",
-      GLRDCL = "",
-      GLRDCR = "",
-      GLRUCL = "",
-      GLRUCR = "",
-    },
-    format = {
-      timestamp = "%H:%M:%S %d-%m-%Y",
-      fields = { "hash", "timestamp", "author", "branch_name", "tag" },
-    },
-    hooks = {
-      on_select_commit = function(commit)
-        vim.notify("DiffviewOpen " .. commit.hash .. "^!")
-        vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
-      end,
-      on_select_range_commit = function(from, to)
-        vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
-        vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
-      end,
-    },
-  },
+        -- Advanced symbols
+        GVER = "",
+        GHOR = "",
+        GCLD = "",
+        GCRD = "╭",
+        GCLU = "",
+        GCRU = "",
+        GLRU = "",
+        GLRD = "",
+        GLUD = "",
+        GRUD = "",
+        GFORKU = "",
+        GFORKD = "",
+        GRUDCD = "",
+        GRUDCU = "",
+        GLUDCD = "",
+        GLUDCU = "",
+        GLRDCL = "",
+        GLRDCR = "",
+        GLRUCL = "",
+        GLRUCR = "",
+      },
+      format = {
+        timestamp = "%H:%M:%S %d-%m-%Y",
+        fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+      },
+      hooks = {
+        on_select_commit = function(commit)
+          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+        end,
+        on_select_range_commit = function(from, to)
+          vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+          vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+        end,
+      },
+    }
+  end,
   keys = {
     {
       "<leader>gl",
