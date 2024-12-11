afd9e490a711aea9ce8853a85eff109553c5c72e
Eslam Allam <eslamallam73@gmail.com>
2024-12-11 23:39:16 +0300

    fix: Use valid search command for miniconda in windows

--- lua/plugins/lang/venv-selector.lua
+++ lua/plugins/lang/venv-selector.lua
@@ -3,18 +3,24 @@ return {
 
   opts = function(_, opts)
     local conda_prefix = os.getenv("CONDA_PREFIX")
+    local path_regex = '/envs/.*/bin/python$'
     if not conda_prefix then
       if vim.fn.has("windows") == 1 then
-        conda_prefix = vim.fn.expandcmd("~/AppData/Local/miniconda3")
+        conda_prefix = vim.fn.expand("~/AppData/Local/miniconda3")
+        path_regex = 'envs//[^//]+//python.exe$'
       else
         error("conda_prefix not found")
       end
     end
+    local function quote(str)
+      return "'" .. str .. "'"
+    end
+    local cmd = { 'fd', quote(path_regex), quote(conda_prefix), '--full-path' }
     return vim.tbl_deep_extend("keep", {
       settings = {
         search = {
           miniconda = {
-            command = "fd '/envs/.*/bin/python$' " .. conda_prefix .. " --full-path",
+            command = table.concat(cmd, " "),
             type = "anaconda",
           },
         },
