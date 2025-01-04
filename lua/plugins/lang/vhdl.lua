5558f8ea6b13fecbf725cb729995babf5146efdf
Eslam Allam <eslamallam73@gmail.com>
2025-01-05 01:23:08 +0300

    feat: Configure vhdl fmt in conform

--- lua/plugins/lang/vhdl.lua
+++ lua/plugins/lang/vhdl.lua
@@ -17,5 +17,17 @@ return {
         vim.list_extend(opts.ensure_installed, { "vhdl" })
       end,
     },
+    {
+      "stevearc/conform.nvim",
+      opts = function(_, opts)
+        opts.formatters_by_ft.vhdl = { "vhdlfmt" }
+        opts.formatters["vhdlfmt"] = {
+          inherit = false,
+          command = "vhdlfmt",
+          args = { "--write", "$FILENAME" },
+          stdin = false,
+        }
+      end,
+    },
   },
 }
