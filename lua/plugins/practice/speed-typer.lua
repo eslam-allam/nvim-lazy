7020f85deed752aa646b56214b4ac12f38989ad3
Eslam Allam <eslamallam73@gmail.com>
2025-02-04 11:42:40 +0300

    feat(speedtyper): Upgrade to v2 branch and adjust setup accordingly

--- lua/plugins/practice/speed-typer.lua
+++ lua/plugins/practice/speed-typer.lua
@@ -1,12 +1,17 @@
 return {
   "NStefan002/speedtyper.nvim",
+  branch = "v2",
   cmd = "Speedtyper",
+  lazy = false,
   keys = {
     { "<localleader>t", "<cmd>Speedtyper<CR>", mode = "n", desc = "Typing Test" },
   },
-  opts = {
-    window = {
-      close_with = { n = "q", i = "<M-q>" },
-    },
-  },
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "speedtyper",
+      callback = function(evt)
+        vim.api.nvim_buf_set_keymap(evt.buf, "n", "q", "<cmd>Speedtyper<CR>", { noremap = true, silent = true })
+      end,
+    })
+  end,
 }
