00e27e1a4332564c18a8e566b00f5250760999b0
Eslam Allam <eslamallam73@gmail.com>
2025-02-25 22:11:31 +0300

    fix: Remove yaml companion

--- lua/plugins/lang/lsp-config.lua
+++ lua/plugins/lang/lsp-config.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "mosheavni/yaml-companion.nvim",
-    ft = "yaml",
-    dependencies = {
-      { "neovim/nvim-lspconfig" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    config = function()
-      local cfg = require("yaml-companion").setup({})
-      require("lspconfig")["yamlls"].setup(cfg)
-      require("telescope").load_extension("yaml_schema")
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = {
-        "<leader>ys",
-        function () require("yaml-companion").open_ui_select() end,
-        desc = "Yaml Schema",
-      }
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       local helpers = require("modules.helpers")
