ab46257de15be4d9ff35be9192060af878631739
Eslam Allam <eslamallam73@gmail.com>
2025-06-15 16:25:03 +0300

    fix: Don't specify dialect when using sqlfluff

---
+++ lua/plugins/lang/sql.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function (_, opts)
+      opts.formatters.sqlfluff = {
+        args = {"format", "-"},
+      }
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        sqlfluff = {
+          args = {
+            "lint",
+            "--format=json",
+          },
+        },
+      },
+    },
+  },
+}
