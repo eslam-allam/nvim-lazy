89817ee8524750658b81ec98b3ce046f2fb003cb
Eslam Allam <eslamallam73@gmail.com>
2025-06-28 02:56:36 +0300

    feat: Ignore wireinject build tag in gopls

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -10,6 +10,15 @@ return {
       opts.servers.htmx = {
         mason = false,
       }
+
+      -- Enable completion in wire template file
+      opts.setup.gopls = function(_, sopts)
+        sopts.settings = {
+          gopls = {
+            buildFlags = { "-tags=wireinject" },
+          },
+        }
+      end
     end,
   },
 }
