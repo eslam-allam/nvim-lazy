18c5e9a217c3b82ca9db41707068d855c150fd41
Eslam Allam <eslamallam73@gmail.com>
2025-07-01 14:17:18 +0300

    fix: Enable sonar lint for many languages

--- lua/plugins/lang/sonar-lint.lua
+++ lua/plugins/lang/sonar-lint.lua
@@ -1,33 +1,35 @@
-local javaExec = require("modules.java").execAtleast(17)
 return {
-  -- Have to configure once per lang as currently languages overlap.
   {
-    url = "https://gitlab.com/schrieveslaach/sonarlint.nvim.git",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-jdtls",
-    },
-    ft = "java",
-    enabled = true,
-    config = function()
-      require("sonarlint").setup({
-        server = {
+    "iamkarasik/sonarqube.nvim",
+    config = function(_, opts)
+      require("sonarqube").setup(opts)
+    end,
+    opts = function()
+      local extension_path =
+        require("plenary.path"):new(vim.fn.expand("$MASON/packages/sonarlint-language-server/extension"))
+      local analyzers = extension_path:joinpath("analyzers")
+      local javaExec = require("modules.java").execAt(17)
+      return {
+        lsp = {
           cmd = {
             javaExec,
             "-jar",
-            vim.fn.expand("$MASON/packages/sonarlint-language-server/extension/server/sonarlint-ls.jar"),
-            -- Ensure that sonarlint-language-server uses stdio channel
+            extension_path:joinpath("server/sonarlint-ls.jar"):absolute(),
             "-stdio",
             "-analyzers",
-            -- paths to the analyzers you need, using those for python and java in this example
-            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+            analyzers:joinpath("sonargo.jar"):absolute(),
+            analyzers:joinpath("sonarhtml.jar"):absolute(),
+            analyzers:joinpath("sonarhtml.jar"):absolute(),
+            analyzers:joinpath("sonariac.jar"):absolute(),
+            analyzers:joinpath("sonarjava.jar"):absolute(),
+            analyzers:joinpath("sonarjavasymbolicexecution.jar"):absolute(),
+            analyzers:joinpath("sonarjs.jar"):absolute(),
+            analyzers:joinpath("sonarphp.jar"):absolute(),
+            analyzers:joinpath("sonarpython.jar"):absolute(),
+            analyzers:joinpath("sonartext.jar"):absolute(),
           },
         },
-        filetypes = {
-          "java",
-        },
-      })
+      }
     end,
   },
 }
