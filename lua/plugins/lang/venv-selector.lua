6341fa7a65048dedeacbc35488b555ace260f249
Eslam Allam <eslamallam73@gmail.com>
2025-07-21 18:12:47 +0300

    feat(python-env): Replace complex search with simplified version

--- lua/plugins/lang/venv-selector.lua
+++ lua/plugins/lang/venv-selector.lua
@@ -1,34 +1,18 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  enabled = true,
+  lazy = false,
   dependencies = {
-    "nvim-telescope/telescope.nvim",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     "nvim-lua/plenary.nvim",
   },
-  opts = function(_, opts)
-    local conda_prefix = os.getenv("CONDA_PREFIX")
-    local path_regex = '/envs/.*/bin/python$'
-    if not conda_prefix then
-      if vim.fn.has("windows") == 1 then
-        conda_prefix = vim.fn.expand("~/AppData/Local/miniconda3")
-        path_regex = 'envs//[^//]+//python.exe$'
-      else
-        error("conda_prefix not found")
-      end
-    end
-    local function quote(str)
-      return "'" .. str .. "'"
-    end
-    local cmd = { 'fd', quote(path_regex), quote(conda_prefix), '--full-path' }
-    return vim.tbl_deep_extend("keep", {
-      settings = {
-        search = {
-          miniconda = {
-            command = table.concat(cmd, " "),
-            type = "anaconda",
-          },
+  opts = {
+    settings = {
+      search = {
+        miniconda_arch = {
+          command = "fd 'bin/python$' /opt/miniconda3/envs --no-ignore-vcs --full-path --color never",
+          type = "anaconda",
         },
       },
-    }, opts)
-  end,
+    },
+  },
 }
