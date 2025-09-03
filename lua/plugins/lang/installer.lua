51adbee74fbc77d0f7bf6498643660729e0f6213
Eslam Allam <eslamallam73@gmail.com>
2025-09-03 18:12:16 +0300

    feat: Use nvim-dbee instead of vim dadbod

--- lua/plugins/lang/installer.lua
+++ lua/plugins/lang/installer.lua
@@ -35,7 +35,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "css", "typescript", "javascript", "scss", "templ", "http" })
+        vim.list_extend(opts.ensure_installed, { "css", "typescript", "javascript", "scss", "templ", "http", "sql" })
       end
     end,
   },
