4a99b59e6a8974cdb5105147aa645089fb4902cb
Eslam Allam <eslamallam73@gmail.com>
2026-01-29 20:27:24 +0300

    feat(snap): Remove padding and watermark

--- lua/plugins/tools/snap.lua
+++ lua/plugins/tools/snap.lua
@@ -4,13 +4,8 @@ return {
   opts = {
     theme = "tmTheme://~/.config/bat/themes/Catppuccin Mocha.tmTheme",
     line_offset = true,
-    watermark = {
-      font = "JetBrainsMono-NF-Regular",
-      font_color = "white",
-      text = " eslam-allam",
-      position = "SouthEast",
-      font_size = 15,
-      opacity = 0.8,
-    },
+    background_colour = "#fff0",
+    pad_h = 0,
+    pad_v = 0,
   },
 }
