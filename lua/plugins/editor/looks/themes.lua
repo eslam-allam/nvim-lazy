4a1c8682ada87636fd210b5fb11a4590ec57a505
Eslam Allam <eslamallam73@gmail.com>
2026-03-29 01:07:29 +0300

    feat: Replace cattpuccin config for tokyonight with transparent background

--- lua/plugins/editor/looks/themes.lua
+++ lua/plugins/editor/looks/themes.lua
@@ -1,59 +1,13 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-    end,
-    lazy = false,
-    priotity = 1000,
-    opts = function(_, opts)
-      local localOpts = {
-        flavour = "mocha",
-        transparent_background = true,
-        integrations = {
-          snacks = {
-            enabled = true,
-            picker_style = "classic",
-          },
-          mason = true,
-          harpoon = true,
-          treesitter = true,
-          notify = true,
-          diffview = true,
-          gitsigns = {
-            enabled = true,
-            transparent = true,
-          },
-          grug_far = true,
-          leap = true,
-          noice = true,
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-              ok = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-              ok = { "underline" },
-            },
-            inlay_hints = {
-              background = true,
-            },
-          },
-          octo = true,
-          which_key = true,
-        },
-      }
-      return vim.tbl_extend("force", opts, localOpts)
-    end,
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
   },
   {
     "LazyVim/LazyVim",
