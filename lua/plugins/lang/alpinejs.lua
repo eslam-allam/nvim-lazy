195537541860e11f5b31a31ee7dc28e3b895ee8b
Eslam Allam <eslamallam73@gmail.com>
2026-06-23 00:51:35 +0300

    feat: Add alpine js plugin

---
+++ lua/plugins/lang/alpinejs.lua
@@ -0,0 +1,8 @@
+return {
+  "connorontheweb/alpinejs.nvim",
+  ft = { "html", "ejs", "php", "twig", "blade", "liquid", "jinja", "htmldjango", "nunjucks" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter", -- optional, recommended
+  },
+  opts = {},
+}
