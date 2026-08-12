9eb1dafc3ece8a786048fcc67811d0e88573e070
Eslam Allam <eslamallam73@gmail.com>
2026-08-12 09:17:26 +0300

    Revert "fix: Remove temple gd workaround"
    This reverts commit 99c0ea04085a49b2dd6f8784b07f62f0b48e9eb6.

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -1,5 +1,38 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local go_to_definition = function()
+        vim.lsp.buf.definition({
+          on_list = function(options)
+            if options == nil or options.items == nil or #options.items == 0 then
+              return
+            end
+
+            local targetFile = options.items[1].filename
+            local prefix = string.match(targetFile, "(.-)_templ%.go$")
+
+            if prefix then
+              local function_name = vim.fn.expand("<cword>")
+              options.items[1].filename = prefix .. ".templ"
+              local qflist = vim.fn.getqflist()
+              vim.fn.setqflist(options.items, " ")
+              vim.api.nvim_command("cfirst")
+              vim.fn.setqflist(qflist, " ")
+              vim.api.nvim_command("silent! /templ " .. function_name)
+            else
+              vim.lsp.buf.definition()
+            end
+          end,
+        })
+      end
+
+      opts.servers.gopls = {
+        keys = { { "gd", go_to_definition, desc = "Goto Definition (Templ Aware)" } },
+      }
+    end,
+  },
+  {
     "maxandron/goplements.nvim",
     ft = "go",
     config = true,
